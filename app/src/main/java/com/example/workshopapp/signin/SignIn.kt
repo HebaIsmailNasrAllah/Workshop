e7f58c7173b9361dd8a736a5c7a5f7a3abf009f1
HebaIsmailNasrAllah <hebahosain@gmail.com>
2021-03-27 18:01:34 +0200

    solved viewModel Issue

--- app/src/main/java/com/example/workshopapp/signin/SignIn.kt
+++ app/src/main/java/com/example/workshopapp/signin/SignIn.kt
@@ -1,27 +1,44 @@
 package com.example.workshopapp.signin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.workshopapp.MainActivity
 import com.example.workshopapp.databinding.ActivitySignInBinding
 
 
 class SignIn : AppCompatActivity() {
     lateinit var binding: ActivitySignInBinding
-    lateinit var userName: String
-    lateinit var passwor: String
     lateinit var signInViewModel: SignInViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        signInViewModel = SignInViewModel()
+        signInViewModel = ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(application)).get(SignInViewModel::class.java)
 
         binding.btnSignIn.setOnClickListener{
-            signInViewModel.validate_login(binding.etSigninUsername.text.toString(), binding.etSigninPassword.text.toString(), this )
+            signInViewModel.validate_login(binding.etSigninUsername.text.toString(), binding.etSigninPassword.text.toString(), application).observe(this, Observer {
+                if(it == 1){
+                    startActivity(Intent(this, MainActivity::class.java))
+                }else if(it == -1){
+                    Toast.makeText(this, "User doesn't exist", Toast.LENGTH_SHORT).show()
+                }else if(it == -2){
+                    Toast.makeText(this, "Ivalid User name or password, Please try again", Toast.LENGTH_SHORT).show()
+                }else if(it == -3){
+                    Toast.makeText(this, "User name and password can't be Empty", Toast.LENGTH_SHORT).show()
+                }
+                Log.i("SignIn", "entered")
+            })
+
         }
+
 
     }
 
 }
+
--- app/src/main/java/com/example/workshopapp/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/workshopapp/signin/SignInViewModel.kt
@@ -1,29 +1,43 @@
 package com.example.workshopapp.signin
 
-import android.content.Context
-import android.content.Intent
+import android.app.Application
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.workshopapp.MainActivity
 import com.example.workshopapp.Utils
+import com.example.workshopapp.localData.ApiObjDataBase
+import com.example.workshopapp.localData.UserEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class SignInViewModel : ViewModel() {
+class SignInViewModel: ViewModel() {
 
-    fun validate_login(userEmail: String, password: String, context: Context) {
-
+    fun validate_login(userEmail: String, password: String, context: Application) :MutableLiveData<Int>{
+        var result= MutableLiveData<Int>()
+        result.value = 0
+        var incomingUser = UserEntity("","", "")
         if (!userEmail.isNullOrEmpty() || !password.isNullOrEmpty()) {
             if (!Utils.validateRegistration(userEmail, password)) {
-                if(userEmail.equals("")){
-                   // context.startActivity(Intent(t))
+                    CoroutineScope(Dispatchers.IO).launch {
+                        incomingUser= ApiObjDataBase.getDatabase(context).userObjDao().getUserByEmail(userEmail)
+                    }
+                if(incomingUser.email.equals(userEmail) && incomingUser.password.equals(password)) {
+                    result.value = 1
                 }else{
-                    Toast.makeText(context, "User doesn't exist", Toast.LENGTH_SHORT).show() }
-            }else{
-                Toast.makeText(context, "Ivalid User name or password, Please try again", Toast.LENGTH_SHORT).show() }
+                    //Toast.makeText(context, "User doesn't exist", Toast.LENGTH_SHORT).show()
+                    result.value = -1
+                 }
+            }else {
+               // Toast.makeText(context, "Ivalid User name or password, Please try again", Toast.LENGTH_SHORT).show()
+                result.value = 2
+            }
         }else{
-            Toast.makeText(context, "User name and password can't be Empty", Toast.LENGTH_SHORT).show() }
-
-
+            //Toast.makeText(context, "User name and password can't be Empty", Toast.LENGTH_SHORT).show()
+            result.value = -3
+               }
+        return result
     }
 
 }
