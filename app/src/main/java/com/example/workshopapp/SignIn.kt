07b55a21038c1c6f8c6ee939ccf5282305f3f911
HebaIsmailNasrAllah <hebahosain@gmail.com>
2021-03-27 12:53:48 +0200

    Sign in

--- app/src/main/java/com/example/workshopapp/SignIn.kt
+++ app/src/main/java/com/example/workshopapp/SignIn.kt
@@ -1,11 +1,33 @@
 package com.example.workshopapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workshopapp.databinding.ActivitySignInBinding
+
 
 class SignIn : AppCompatActivity() {
+    lateinit var binding: ActivitySignInBinding
+    lateinit var userName: String
+    lateinit var passwor: String
+    lateinit var signInViewModel: SignInViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        signInViewModel = SignInViewModel()
+
+        binding.btnSignIn.setOnClickListener{
+            signInViewModel.validate_login(binding.etSigninUsername.text.toString(), binding.etSigninPassword.text.toString(), this )
+        }
+
+
+
+
+
     }
+
+
 }
