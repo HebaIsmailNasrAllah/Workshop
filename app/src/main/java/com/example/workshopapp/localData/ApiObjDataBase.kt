b8a7d55f3ab1ebc656c86e72c2dc807fc408fe99
Doaa Gomaa <dgomaa49@gmail.com>
2021-03-27 12:03:27 +0200

    adding register design and userEntity and DataBase Class

---
+++ app/src/main/java/com/example/workshopapp/localData/ApiObjDataBase.kt
@@ -0,0 +1,33 @@
+package com.example.workshopapp.localData
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities =[ UserEntity::class] ,version = 1)
+abstract class ApiObjDataBase : RoomDatabase() {
+    // abstract fun apiObjDao(): NewsResponseDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: ApiObjDataBase? = null
+
+        fun getDatabase(application: Application): ApiObjDataBase {
+            // if the INSTANCE is not null, then return it,
+            // if it is, then create the database
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        application.applicationContext,
+                        ApiObjDataBase::class.java,
+                        "NewsDB"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                // return instance
+                instance
+            }
+        }
+    }
+
+}
