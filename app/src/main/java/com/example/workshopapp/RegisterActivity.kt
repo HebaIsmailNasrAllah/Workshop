742c1be7c68ee5d8d09b2326926b5f544afdb83a
Doaa Gomaa <dgomaa49@gmail.com>
2021-03-27 18:02:28 +0200

    registration feature done

--- app/src/main/java/com/example/workshopapp/RegisterActivity.kt
+++ app/src/main/java/com/example/workshopapp/RegisterActivity.kt
@@ -1,10 +1,15 @@
 package com.example.workshopapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.workshopapp.databinding.ActivityRegisterBinding
 import com.example.workshopapp.localData.viewModel.RegisterViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding:ActivityRegisterBinding
@@ -17,6 +22,15 @@ class RegisterActivity : AppCompatActivity() {
          binding =ActivityRegisterBinding .inflate(layoutInflater)
                setContentView(binding.root)
         registerViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(RegisterViewModel::class.java)
-        registerViewModel.validateUser(binding.)
+
+        binding.buttonSignUp.setOnClickListener {
+            registerViewModel.validateUser(binding.editUserName.text.toString(),binding.editPasswordSignUp.text.toString())
+
+            var intent = Intent(this@RegisterActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+
+        }
     }
 }
